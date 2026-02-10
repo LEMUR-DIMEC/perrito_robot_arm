b53037ea3e8ced6995c7229a0b45ea0cb3b4e8cd
uwulises <ulises.campodonico@ug.uchile.cl>
2026-02-10 13:50:07 -0300

    add rate limiting

--- sim/test.py
+++ sim/test.py
@@ -2,20 +2,16 @@ import mujoco
 import mujoco.viewer
 import time
 import numpy as np
+from loop_rate_limiters import RateLimiter
 
 model = mujoco.MjModel.from_xml_path("sim/model/scene.xml")
 data = mujoco.MjData(model)
 model.opt.gravity = (0, 0, -9.81)
 mujoco.mj_resetDataKeyframe(model, data, 0)
-with mujoco.viewer.launch(model, data, show_left_ui=True,show_right_ui=True) as viewer:
-  start = time.time()
+with mujoco.viewer.launch_passive(model, data, show_left_ui=True,show_right_ui=True) as viewer:
+  rate = RateLimiter(frequency=2000.0, warn=False)
   while viewer.is_running():
-    step_start = time.time()
-    #data.ctrl = np.random.randn(model.nu)
     mujoco.mj_step(model, data)
     # Pick up changes to the physics state, apply perturbations, update options from GUI.
     viewer.sync()
-    # Rudimentary time keeping, will drift relative to wall clock.
-    time_until_next_step = model.opt.timestep - (time.time() - step_start)
-    if time_until_next_step > 0:
-      time.sleep(time_until_next_step)
+    rate.sleep()
